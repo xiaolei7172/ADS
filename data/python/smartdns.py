78e05a3556ccc81ba98397f4e839dbba638b036d
天影丶 <62822462+xiaolei7172@users.noreply.github.com>
2026-05-09 10:12:05 +0800

    Update smartdns.py

--- data/python/smartdns.py
+++ data/python/smartdns.py
@@ -12,8 +12,8 @@ def convert_to_smartdns_format(input_file, output_file):
     count = 0
     with open(output_file, 'w') as file:
         # Add header comment
-        file.write("# SmartDNS rules for GOODBYEADS\n")
-        file.write("# Homepage: https://github.com/8680/GOODBYEADS\n")
+        file.write("# SmartDNS rules for 天影\n")
+        file.write("# Homepage: https://github.com/xiaolei7172/ADS\n")
         file.write("# Format: address /domain/#\n\n")
         
         for line in lines:
