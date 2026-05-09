1919ecc8d3cd9d4925ef10f175f2bdf0ad4a2ae5
天影丶 <62822462+xiaolei7172@users.noreply.github.com>
2026-05-09 10:29:46 +0800

    Update dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -50,7 +50,7 @@ adblock = [
     "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
     "https://raw.githubusercontent.com/2771936993/HG/main/hg1.txt",
     "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
-    "https://raw.githubusercontent.com/changzhaoCZ/fqnovel-adrules/refs/heads/main/fqnovel-fxxk_ads",
+    #"https://raw.githubusercontent.com/changzhaoCZ/fqnovel-adrules/refs/heads/main/fqnovel-fxxk_ads",
     "https://anti-ad.net/adguard.txt",
     "https://raw.githubusercontent.com/2Gardon/SM-Ad-FuckU-hosts/master/SMAdHosts",
     "https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt"
