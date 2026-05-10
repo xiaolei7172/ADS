a8ea2e6379e41b4bc5e41371e1bc8c3bcdaae97f
天影丶 <62822462+xiaolei7172@users.noreply.github.com>
2026-05-10 21:57:51 +0800

    Update dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -35,15 +35,15 @@ subprocess.run("cp ./data/mod/whitelist.txt ./tmp/allow01.txt", shell=True)
 
 # 拦截规则
 adblock = [
-"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",  # AdguardTeam 官方基础广告拦截规则
-"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",  # AdguardTeam 官方中文专属广告规则
-"https://easylist-downloads.adblockplus.org/easyprivacy.txt",  # EasyPrivacy 隐私跟踪防护规则
+#"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",  # AdguardTeam 官方基础广告拦截规则
+#"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",  # AdguardTeam 官方中文专属广告规则
+#"https://easylist-downloads.adblockplus.org/easyprivacy.txt",  # EasyPrivacy 隐私跟踪防护规则
 "https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_small.txt",  # sjhgvr OISD精简版广告规则
-"https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",  # TG-Twilight 秋风广告规则
+#"https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",  # TG-Twilight 秋风广告规则
 #"https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",  # cjx82630 网页烦扰元素拦截规则
 "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt",  # afwfv DD-AD 综合广告拦截规则
 #"https://ghproxy.net/https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/dns.txt",  # 消失的DD DNS精简版
-"https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",  # damengzhu 广告接口拦截规则
+#"https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",  # damengzhu 广告接口拦截规则
 "https://raw.githubusercontent.com/2771936993/HG/main/hg1.txt",  # HG 综合广告/弹窗拦截规则
 "https://raw.githubusercontent.com/xiaolei7172/ADS/refs/heads/master/data/mod/adblock.txt",  # 天影自用补充规则
 "https://anti-ad.net/easylist.txt",  # anti-AD 命中率高、兼容性强
@@ -52,13 +52,13 @@ adblock = [
 #"https://ghproxy.net/https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/light.txt",  # hagezi 轻量低误杀DNS规则
 #"https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt",  # 极客 DNS专用规则
 #"https://raw.gitmirror.com/Cats-Team/dns-filter/main/abp.txt",  # AdRules DNS Filter 国内优化
-"https://raw.hellogithub.com/hosts",  # GitHub加速Hosts
+#"https://raw.hellogithub.com/hosts",  # GitHub加速Hosts
 #"https://raw.githubusercontent.com/entr0pia/fcm-hosts/fcm/fcm-hosts",  # FCM Hosts 隐私推送优化
 "https://www.xlxbk.cn/dns.txt",  # xlxbj_dns 国内短视频APP广告域名
-"https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/Master/FuLingRules/FuLingBlockList.txt",  # 🌸茯苓广告规则
+#"https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/Master/FuLingRules/FuLingBlockList.txt",  # 🌸茯苓广告规则
 #"https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt",  # 当初 adhosts 域名拦截
-"https://mirror.ghproxy.com/raw.githubusercontent.com/qq5460168/666/master/rules.txt",  # 群主合并规则
-"https://get.66a.vip/https://raw.githubusercontent.com/sccheng460/me/master/rules.txt"  # 知还管理 合并规则
+#"https://mirror.ghproxy.com/raw.githubusercontent.com/qq5460168/666/master/rules.txt",  # 群主合并规则
+#"https://get.66a.vip/https://raw.githubusercontent.com/sccheng460/me/master/rules.txt"  # 知还管理 合并规则
 ]
 
 # 白名单规则
