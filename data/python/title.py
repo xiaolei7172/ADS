f2f91705665a53d1af53f1987a4f6792aef0f932
天影丶 <62822462+xiaolei7172@users.noreply.github.com>
2026-05-10 22:19:46 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -34,6 +34,7 @@ for file_path in file_list:
     f"! Homepage:🌐 https://github.com/xiaolei7172/ADS\n"
     f"! Expires: ⏰ 每 12 小时自动更新\n"
     f"! Description: 📝 更新时间：{beijing_time}（北京时间）｜📝 当前规则总数：{line_count} 条｜\n"
+    f"! Total count:{line_count}
     f"! ======================================================================\n"
         f"{content}"
     )
